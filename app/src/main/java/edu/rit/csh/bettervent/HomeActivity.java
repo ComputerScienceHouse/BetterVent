f7b10b078675038bf47c6afbf4deb33d76b121db
Will <wdn5796.rit.edu>
2018-12-01 15:27:16 -0500

    Initial commit. Barebones, not even a thing right now. Throwing it up here to force myself to work on it. December Hackathon, GO!

---
+++ app/src/main/java/edu/rit/csh/bettervent/HomeActivity.java
@@ -0,0 +1,52 @@
+package edu.rit.csh.bettervent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private ConstraintLayout mHomeLayout;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) { // What to do if each of the three buttons are pressed.
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    mHomeLayout.setVisibility(View.VISIBLE);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    mHomeLayout.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    mHomeLayout.setVisibility(View.INVISIBLE);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        mHomeLayout = (ConstraintLayout) findViewById(R.id.HomeLayout);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
